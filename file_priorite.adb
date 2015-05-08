2021095d7620f1cc7b6dd243b034d64302e0e0fa
RomainD <romain.dulery@ensimag.grenoble-inp.fr>
2015-05-08 17:36:16 +0200

    Ajout files priorité

--- file_priorite.adb
+++ file_priorite.adb
@@ -1,29 +1,62 @@
-
-With Ada.Text_IO;
+with Ada.Text_IO;
 use Ada.Text_IO;
 
 package body File_Priorite is
 
-	function Nouvelle_File(Taille: Positive) return File is
-		F: File;
-	begin
-		--TODO
-		return F;
-	end Nouvelle_File;
+   function Nouvelle_File(Taille: Positive) return File is
+      F: File;
+   begin
+      F := new File_Interne(Taille); 
+      return F;
+   end Nouvelle_File;
 
-	procedure Insertion(F: in out File; P: Priorite; D: Donnee) is
-	begin
-		null; --TODO
-	end Insertion;
+   procedure Insertion(F: in out File; P: Priorite; D: Donnee) is
+      i : Integer;
+      Mem : Cellule;
+   begin
+      F.Last := F.Last + 1;
+      F.Tas(F.Last) := (D,P);
+      i := F.last;
+      while (i>1) and Compare(F.Tas(i).Prio,F.Tas(i/2).Prio) = INF loop
+	 Mem := F.Tas(i);
+	 F.Tas(i) := F.tas(i/2);
+	 F.Tas(i/2) := Mem;
+      end loop;
 
-	procedure Meilleur(F: in File; P: out Priorite; D: out Donnee; Statut: out Boolean) is
-	begin
-		null; --TODO
-	end;
+   end Insertion;
 
-	procedure Suppression(F: in out File) is
-	begin
-		null; --TODO
-	end;
+   procedure Meilleur(F: in File; P: out Priorite; D: out Donnee; Statut: out Boolean) is
+   begin
+      if F.Last = 0 then
+	 Statut := False;
+      else
+	 D := F.Tas(1).Data;
+	 P := F.Tas(1).Prio;
+	 Statut := True;
+      end if;
+   end;
 
-end;
+   procedure Suppression(F: in out File) is
+      i,j : Integer;
+   begin
+      F.Tas(1) := F.Tas(F.Last);
+      i := 1;
+      while i < (F.Last/2) loop
+	 if 2*i = F.Last or Compare(F.Tas(2*i).Prio,F.Tas(2*i+1).Prio) = INF then
+	    j := 2*i;
+	 else
+	    j := 2*i+1;
+	 end if;
+	 if Compare(F.Tas(i).Prio,F.Tas(j).Prio) = SUP then
+	    declare
+	       Mem : Cellule := F.Tas(i);
+	    begin
+	       F.Tas(i) := F.Tas(j);
+	       F.Tas(j) := Mem;
+	    end;
+	 else
+	    return;
+	 end if;
+      end loop;
+   end;
+end File_Priorite;
--- file_priorite.ads
+++ file_priorite.ads
@@ -5,26 +5,38 @@ use Comparaisons;
 
 generic
 
-        type Priorite is private;
-        with function Compare(C1, C2: Priorite) return Comparaison;
+type Priorite is private;
+with function Compare(C1, C2: Priorite) return Comparaison;
 
-        type Donnee is private;
+type Donnee is private;
 
 package File_Priorite is
 
-        type File is private;
+   type File is private;
 
-		--cree une file pouvant contenir au plus 'Taille' elements
-        function Nouvelle_File(Taille: Positive) return File;
-		--insere un element de la priorite donnee
-        procedure Insertion(F: in out File; P: Priorite; D: Donnee);
-		--recupere l'element de meilleure priorite ; met le statut a Faux si la file est vide
-		--le laisse dans la file
-        procedure Meilleur(F: in File; P: out Priorite; D: out Donnee; Statut: out Boolean);
-		--supprime l'element de meilleure priorite
-        procedure Suppression(F: in out File);
+   --cree une file pouvant contenir au plus 'Taille' elements
+   function Nouvelle_File(Taille: Positive) return File;
+   --insere un element de la priorite donnee
+   procedure Insertion(F: in out File; P: Priorite; D: Donnee);
+   --recupere l'element de meilleure priorite ; met le statut a Faux si la file est vide
+   --le laisse dans la file
+   procedure Meilleur(F: in File; P: out Priorite; D: out Donnee; Statut: out Boolean);
+   --supprime l'element de meilleure priorite
+   procedure Suppression(F: in out File);
 
-private
-        type File_Interne;
-        type File is access File_Interne;
+   private
+   type File_Interne;
+   type File is access File_Interne;
+
+   type Cellule is record
+      Data : Donnee ;
+      Prio : Priorite ;
+   end record ;
+
+   type Tab is array(Integer range <>) of Cellule;
+
+   type File_Interne(Taille : Positive) is record
+      Last : Integer := 0;
+      Tas : Tab(1..Taille);
+   end record ;
 end;
